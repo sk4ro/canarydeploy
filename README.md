73dc69737eef7eb194992bad160db6f9f4684e91
sk4ro <sk4ro@protonmail.ch>
2016-09-23 00:48:34 +0000

    Complete redesign, satisfied for now

--- README.md
+++ README.md
@@ -1,30 +1,27 @@
 #CanaryDeploy
-Linux autodeploy scripts for OpenCanary (https://github.com/thinkst/opencanary)
-and opencanary-correlator (https://github.com/thinkst/opencanary-correlator)
+Autodeploy script for OpenCanary (https://github.com/thinkst/opencanary)
 #Overview
 Tested on CentOS 7.0 and XUbuntu 16.04 LTS.
 
 Useful documentation resides at http://www.opencanary.org/en/latest/
 
+Uses .opencanary.conf generated in your home directory on first run.
+
+After script completes, edit as necessary.
+
+Set up a centralized correlator using the README at https://github.com/thinkst/opencanary-correlator
+
 #Requirements - opencanary
 <ul>
 <li>Python 2.7</li>
 <li>python-pip</li>
-<li>python-devel</li>
+<li>python-devel (CentOS)</li>
+<li>python-dev (Ubuntu)</li>
 <li>python-virtualenv</li>
 <li>[Optional] SNMP requires scapy</li>
 <li>[Optional] RDP requires rdpy</li>
 <li>[Optional] Samba module needs a working installation of samba</li>
 <li>[Ubuntu dependencies (if using rdpy)]: build-essential libssl-dev libffi-dev python-dev</li>
-</ul>
-
-#Requirements - opencanary-correlator
-<ul>
-<li>Python 2.7</li>
-<li>Redis</li>
-<li>Mandrill API keys for email</li>
-<li>Twillio API keys for SMS</li>
-<li>[Ubuntu-specific dependencies]: redis-server libffi-dev python-dev</li>
 </ul>
 
 #Install - opencanary
@@ -35,15 +32,6 @@ $ . envname/bin/activate</code></pre>
 Next, we invoke this command to install:
 
 <pre><code>$ sh -c "$(curl -fsSL https://raw.githubusercontent.com/sk4ro/canarydeploy/master/deploy.sh)"</code></pre>
-
-#Install - opencanary-correlator
-Same thing here, create a virtual environment:
-<pre><code>$ virtualenv envname/
-$ . envname/bin/activate</code></pre>
-
-Install via this command:
-
-<pre><code>$ sh -c "$(curl -fsSL https://raw.githubusercontent.com/sk4ro/canarydeploy/master/correlator.sh")</code></pre>
 
 #To-Do:
 <ul>
--- deploy.sh
+++ deploy.sh
@@ -1,48 +1,49 @@
 #!/bin/bash
 #-Metadata----------------------------------------------------#
-#   Filename: skynet.sh                (Update: 2016-06-29)   #
+#   Filename: deploy.sh                (Updated: 2016-09-22)  #
 #-Info--------------------------------------------------------#
-#   Linux auto-deploy script for opencanary-correlator        #
-#   (https://github.com/thinkst/opencanary-correlator)        #
+#   Auto-deploy script for opencanary                         #
+#   (https://github.com/thinkst/opencanary)                   #
 #-Author(s)---------------------------------------------------#
 #   sk4ro - https://twitter.com/AnarchistDalek                #
-#   Some features appropriated from g0tmi1k's os-scripts      #
+#   Colors culturally appropriated from g0tmi1k's os-scripts  #
 #   (https://github.com/g0tmi1k/os-scripts)                   #
-#-Operating System--------------------------------------------#
-#   Designed for: CentOS 7.0 (VM - Virtualbox)                #
-#   Tested on: CentOS 7.0 VM, XUbuntu 16.04 VM                #
-#   PLACEHOLDER FOR OTHER OS FUNCTIONALITY                    #
-#   PLACEHOLDER FOR OTHER OS FUNCTIONALITY                    #
+#-Operating Systems-------------------------------------------#
+#   CentOS 7, Debian family, OSX (Macports/Homebrew)          #
+#	Tested on: CentOS 7, Ubuntu 16.04 LTS                     #
 #-License-----------------------------------------------------#
 #   MIT License ~ http://opensource.org/licenses/MIT          #
 #-Notes-------------------------------------------------------#
-#                                                             #
 #   Start this script from an active virtualenv.              #
-#                                                             #
 #-------------------------------------------------------------#
 
-##### Pretty colors because why not
-RED="\033[01;31m"      # Issues/Errors
-GREEN="\033[01;32m"    # Success
-YELLOW="\033[01;33m"   # Warnings/Information
-BLUE="\033[01;34m"     # Heading
-BOLD="\033[01;01m"     # Highlight
-RESET="\033[00m"       # Normal
-#####
+# Pretty colors because why not
+RED="\033[01;31m"
+GREEN="\033[01;32m"
+YELLOW="\033[01;33m"
+BLUE="\033[01;34m"
+BOLD="\033[01;01m"
+RESET="\033[00m"
 
-# Command to confirm installation, in case user wishes to exit gracefully
-confirm () {
-		echo -e " ${YELLOW}[?]${RESET} ${BOLD}Do you wish to continue? [y/N]${RESET}"
-		read response
-		case $response in
-			[yY][eE][sS]|[yY])
-				true
-				;;
-		*)
-				false
-				exit 1
-				;;
-		esac		
+function deploy {
+		printf "${YELLOW}[?]${RESET} ${BOLD}Please enter the name of this canary:${RESET} "
+
+		read canary
+
+		pip install opencanary
+
+		printf "${BLUE}[*]${RESET} ${BOLD}Installation complete. Generating config and applying SPACE MAGIC!${RESET} \n"
+
+		# Sed in-place the generated config template to name the canary
+		# Also seds in-place 9 lines of malformed hpfeeds irrelevance explored further at
+		# https://github.com/thinkst/opencanary/issues/9
+		opencanaryd --copyconfig && sed -i 's/opencanary-1/'$canary'/' ~/.opencanary.conf && sed -i 58,66d ~/.opencanary.conf
+
+		printf "${GREEN}[!]${RESET} ${BOLD}Starting opencanaryd. (Stop with opencanaryd --stop) ${RESET} \n"
+
+		opencanaryd --start
+
+		exit 1
 }
 
 # Select host OS
@@ -51,52 +52,34 @@ ospick=("CentOS" "Ubuntu" "OSX (Macports)" "OSX (Homebrew)" "GTFO")
 select gibs in "${ospick[@]}"
 do
 	case $gibs in
-		"1")
-			echo "You have chosen CentOS. Installing... "
+		"CentOS")
+			printf "${BOLD}You have chosen CentOS. Installing... \n${RESET}"
+			deploy
 			;;
-		"2")
-			echo "You have chosen Debian/Ubuntu. Installing dependencies... "
+		"Ubuntu")
+			printf "${BOLD}You have chosen Debian/Ubuntu. Installing dependencies... \n${RESET}"
 			sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev
+			deploy
 			;;
-		"3")
-			echo "You have chosen OSX (Macports). Installing dependencies... "
+		"OSX (Macports)")
+			printf "${BOLD}You have chosen OSX (Macports). Installing dependencies... \n${RESET}"
 			sudo port install openssl
 			env ARCHFLAGS="-arch x86_64" LDFLAGS="-L/opt/local/lib" CFLAGS="-I/opt/local/include" pip install cryptography
+			deploy
 			;;
-		"4")
-			echo "You have chosen OSX (Homebrew). Installing dependencies... "
+		"OSX (Homebrew)")
+			printf "${BOLD}You have chosen OSX (Homebrew). Installing dependencies... \n${RESET}"
 			brew install openssl
 			env ARCHFLAGS="-arch x86_64" LDFLAGS="-L/usr/local/opt/openssl/lib" CFLAGS="-I/usr/local/opt/openssl/include" pip install cryptography
+			deploy
 			;;
-		"5")
-			echo "Exiting. "
-			break
-			;;
-		"")
-			echo "Please enter a number between 1 and 5. "
+		"GTFO")
+			printf "${BOLD}Exiting. \n${RESET}"
 			exit 1
 			;;
 		*)
-			echo "Please enter a number between 1 and 5. "
+			printf "${BOLD} Please enter a number between 1 and 5. \n${RESET}"
 			exit 1
 			;;
 	esac
 done
-
-echo -e " ${YELLOW}[?]${RESET} ${BOLD}Please enter the name of this canary:${RESET} "
-
-read canary
-
-pip install opencanary > /dev/null 2>&1 # Install opencanary via pip, suppressing installer output
-
-echo -e " ${BLUE}[*]${RESET} ${BOLD}Installation complete. Generating config and applying SPACE MAGIC!${RESET} "
-
-# Generate config while suppressing output, replace default canary name with user input, remove hpfeeds stuff
-# Sed in-place the generated config template to name the canary
-# Also seds in-place 9 lines of malformed hpfeeds irrelevance explored further at 
-# https://github.com/thinkst/opencanary/issues/9
-opencanaryd --copyconfig > /dev/null 2>&1 && sed -i 's/opencanary-1/'$canary'/' ~/.opencanary.conf && sed -i 58,66d ~/.opencanary.conf
-
-echo -e " ${GREEN}[!]${RESET} ${BOLD}Stay safe out there. (Stop with opencanaryd --stop) ${RESET} "
-
-opencanaryd --start
